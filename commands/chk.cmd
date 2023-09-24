fe9e1aaef5f926367d395c66c1d5f901fc446e1e
Lucas Gabriel (lucmsilva) <lucmsilva651@gmail.com>
2023-09-23 22:40:43 -0300

    Update chk.cmd

--- commands/chk.cmd
+++ commands/chk.cmd
@@ -5,11 +5,14 @@ set /p target="Enter the name of the file or folder: "
 
 if exist "!target!" (
     if exist "!target!\*" (
+        echo.
         echo The directory or folder !target! exists.
     ) else (
+        echo.
         echo The file !target! exists.
     )
 ) else (
+    echo.
     echo The file or folder !target! does NOT exist.
 )
 
