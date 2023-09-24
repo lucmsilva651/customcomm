4effa258ccc87f7b45b1e5cca4e6624b9ed38b53
Lucas Gabriel (lucmsilva) <lucmsilva651@gmail.com>
2023-09-23 22:09:45 -0300

    Update cchelp.cmd

--- commands/cchelp.cmd
+++ commands/cchelp.cmd
@@ -12,6 +12,6 @@ echo     - ccver - Help command. Same as "cchelp".
 echo     - ls - List files in the directory. Usage: ls ^<folder^>
 echo     - back - Go to the parent folder.
 echo     - clear - Clear the terminal.
-echo     - display - Display ".txt" files. Usage: display ^<file^>
+echo     - display - Display the text of a file. Usage: display ^<file^>
 echo     - nav - Browse folders. Usage: nav ^<folder^>
 echo     - ln - Creates a symbolic link. Usage: ln ^<file^>
